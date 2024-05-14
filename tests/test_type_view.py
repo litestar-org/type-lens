3883af73095bf2e18065641742e7836301a2a176
DanCardin <DanCardin@users.noreply.github.com>
2024-05-14 11:08:33 -0400

    feat: Add repr to TypeView that mirrors the input. (#13)

--- tests/test_type_view.py
+++ tests/test_type_view.py
@@ -6,6 +6,7 @@ from typing import (
     Annotated,
     Any,
     ForwardRef,
+    Literal,
     Optional,
     TypedDict,
     TypeVar,
@@ -292,3 +293,9 @@ def test_tuple():
 
     assert TypeView(tuple[int, ...]).is_tuple is True
     assert TypeView(tuple[int, ...]).is_variadic_tuple is True
+
+
+def test_repr():
+    assert repr(TypeView(int)) == "TypeView(int)"
+    assert repr(TypeView(Optional[str])) == "TypeView(typing.Optional[str])"
+    assert repr(TypeView(Literal["1", 2, True])) == "TypeView(typing.Literal['1', 2, True])"
