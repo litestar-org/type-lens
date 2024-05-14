c0e5f9ce5c1cb08d1ffbfbdf3d197a1b2db78100
DanCardin <ddcardin@gmail.com>
2024-05-13 20:26:42 -0400

    feat: Add unwrap_optional method to TypeView.

--- tests/test_type_view.py
+++ tests/test_type_view.py
@@ -292,3 +292,12 @@ def test_tuple():
 
     assert TypeView(tuple[int, ...]).is_tuple is True
     assert TypeView(tuple[int, ...]).is_variadic_tuple is True
+
+
+def test_strip_optional() -> None:
+    # Non-optionals should return the original input
+    assert TypeView(int).strip_optional() == TypeView(int)
+
+    assert TypeView(Optional[int]).strip_optional() == TypeView(int)
+    assert TypeView(Optional[Union[str, int]]).strip_optional() == TypeView(Union[str, int])
+    assert TypeView(Union[str, int, None]).strip_optional() == TypeView(Union[str, int])
