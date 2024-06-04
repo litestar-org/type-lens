e79b8f23451c554419a6272994675e7e00bde661
DanCardin <DanCardin@users.noreply.github.com>
2024-06-03 21:15:56 -0400

    feat: Expose the Empty and EmptyType types. (#24)

--- type_lens/__init__.py
+++ type_lens/__init__.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 from .parameter_view import ParameterView
 from .type_view import TypeView
+from .types.empty import Empty, EmptyType
 
 __all__ = (
     "ParameterView",
     "TypeView",
+    "EmptyType",
+    "Empty",
 )
