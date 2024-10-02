cd6552c25bff62fe8c78519c0011ec4713bcbe18
DanCardin <ddcardin@gmail.com>
2024-10-02 06:52:32 -0400

    feat: Add has_annotation field to ParameterView.

--- type_lens/parameter_view.py
+++ type_lens/parameter_view.py
@@ -23,18 +23,35 @@ class ParameterView:
         "name": "The name of the parameter.",
         "default": "The default value of the parameter.",
         "type_view": "View of the parameter's annotation type.",
+        "has_annotation": (
+            "Whether the parameter had an annotation or not. Lack of an annotation implies "
+            "`TypeView(Any)`, but that is distinct from being explicitly `Any` annotated."
+        ),
     }
 
-    def __init__(self, name: str, type_view: TypeView = _any_type_view, *, default: Any | EmptyType = Empty) -> None:
+    def __init__(
+        self,
+        name: str,
+        type_view: TypeView = _any_type_view,
+        *,
+        default: Any | EmptyType = Empty,
+        has_annotation: bool = True,
+    ) -> None:
         self.name: Final = name
         self.type_view: Final = type_view
         self.default: Final = default
+        self.has_annotation: Final = has_annotation
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, ParameterView):
             return False
 
-        return bool(self.name == other.name and self.type_view == other.type_view and self.default == other.default)
+        return bool(
+            self.name == other.name
+            and self.type_view == other.type_view
+            and self.default == other.default
+            and self.has_annotation == other.has_annotation
+        )
 
     def __repr__(self) -> str:
         cls_name = self.__class__.__name__
@@ -68,5 +85,6 @@ class ParameterView:
         return cls(
             name=parameter.name,
             default=Empty if parameter.default is Signature.empty else parameter.default,
+            has_annotation=parameter.annotation is not Signature.empty,
             type_view=TypeView(annotation),
         )
